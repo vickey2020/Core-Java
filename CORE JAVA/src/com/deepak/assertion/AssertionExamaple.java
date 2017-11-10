0e179ae05ccb568c4ba71b8fb507d8a3ded2688a
deepak <deepak@192.168.2.8>
2017-11-10 13:01:18 +0530

    core java

---
+++ CORE JAVA/src/com/deepak/assertion/AssertionExamaple.java
@@ -0,0 +1,9 @@
+package com.deepak.assertion;
+
+public class AssertionExamaple {
+	public static void main(String[] args) {
+		int i = 12;
+		assert (i == 13) : "problem in integer type";
+		System.out.println("Done");
+	}
+}
